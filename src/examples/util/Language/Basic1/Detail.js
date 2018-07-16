62923d11a9019ceb27345831ccb0601950fcb759
liuwenzhu <547485261@qq.com>
2018-07-16 16:24:14 +0800

    添加多语言
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/examples/util/Language/Basic1/Detail.js
+++ src/examples/util/Language/Basic1/Detail.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-import { CountDown } from 'quant-ui';
+import { language ,Button} from 'quant-ui';
+const $ = language.getLanguageData;
 
-const targetTime = new Date().getTime() + 3900000;
+
+language.setLanguageData({
+	'en_US':{
+		"你好！":"hello！",
+		"语言":"language",
+	},
+	'ja_JP':{
+		"你好！":"あろは",
+		"语言":"じ",
+	}
+})
 class Detail extends Component {
 
+	zhClick = () =>{
+		language.setCurrentLanguage("zh_CN")
+		language.refreshLanguage()
+	}
+	enClick = () =>{
+		language.setCurrentLanguage("en_US")
+		language.refreshLanguage()
+	}
+	jaClick = () =>{
+		language.setCurrentLanguage("ja_JP")
+		language.refreshLanguage()
+	}
 	render() {
 		return (
 			<div>
-				 <CountDown style={{ fontSize: 20 }} target={targetTime} />
+				<Button onClick={this.zhClick}>中文</Button>
+				<Button onClick={this.enClick}>英文</Button>
+				<Button onClick={this.jaClick}>日语</Button>
+				<div>
+					{$("你好！")} 
+					<br /><br />
+					{$("语言")}
+				</div>
 			</div>
+			
 		);
 	}
 }
--- src/examples/util/Language/api.js
+++ src/examples/util/Language/api.js
@@ -5,23 +5,44 @@ const api = [
 		api: [
 			{
 				key: "1",
-				parameter: "format",  //字段名
-				instructions: '时间格式化显示',	//描述
-				type: '	Function(time)',	//类型
+				parameter: "getAllLanguage",  //字段名
+				instructions: '获取所有语言数据',	//描述
+				type: 'function()',	//类型
 				default: "-",							//默认值
 				must: '',
 			},{
 				key: "2",
-				parameter: "target",  //字段名
-				instructions: '目标时间',	//描述
-				type: 'Date',	//类型
-				default: "",							//默认值
+				parameter: "getCurrentLanguage",  //字段名
+				instructions: '获取当前语言类型',	//描述
+				type: 'function',	//类型
+				default: "zh_CN",							//默认值
 				must: '',
 			},{
 				key: "3",
-				parameter: "onEnd",  //字段名
-				instructions: '	倒计时结束回调',	//描述
+				parameter: "getLanguageData",  //字段名
+				instructions: '	获取根据当前预览类型翻译后的语言',	//描述
 				type: 'funtion',	//类型
+				default: "",							//默认值
+				must: '',
+			},{
+				key: "4",
+				parameter: "refreshLanguage",  //字段名
+				instructions: '	刷新语言',	//描述
+				type: 'funtion',	//类型
+				default: "",							//默认值
+				must: '',
+			},{
+				key: "5",
+				parameter: "setCurrentLanguage",  //字段名
+				instructions: '	设置当前语言类型',	//描述
+				type: 'funtion（String[语言类型]）',	//类型
+				default: "",							//默认值
+				must: '',
+			},{
+				key: "6",
+				parameter: "setLanguageData",  //字段名
+				instructions: '	设置语言数据',	//描述
+				type: 'funtion（Object）',	//类型
 				default: "",							//默认值
 				must: '',
 			}
