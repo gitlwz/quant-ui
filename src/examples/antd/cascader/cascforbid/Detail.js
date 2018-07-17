8eb092f63caff058d399f4b036405df039f06710
yueyong <1397604589@qq.com>
2018-07-17 17:51:48 +0800

    Cascader级联选择 部分文档
    Signed-off-by: yueyong <1397604589@qq.com>

---
+++ src/examples/antd/cascader/cascforbid/Detail.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { Cascader } from 'quant-ui';
+
+const options = [{
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [{
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [{
+            value: 'xihu',
+            label: 'West Lake',
+        }],
+    }],
+}, {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    disabled: true,
+    children: [{
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [{
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+        }],
+    }],
+}];
+
+function onChange(value) {
+    console.log(value);
+}
+class Detail extends Component {
+    render() {
+        return (
+            <Cascader options={options} onChange={onChange} />
+        );
+    }
+}
+export default Detail;
---
+++ src/examples/antd/cascader/choAndcha/Detail.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Cascader } from 'quant-ui';
+
+const options = [{
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [{
+        value: 'hangzhou',
+        label: 'Hanzhou',
+        children: [{
+            value: 'xihu',
+            label: 'West Lake',
+        }],
+    }],
+}, {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [{
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [{
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+        }],
+    }],
+}];
+
+function onChange(value) {
+    console.log(value);
+}
+
+class Detail extends Component {
+
+    render() {
+        return (
+            <Cascader options={options} onChange={onChange} changeOnSelect />
+        );
+    }
+}
+export default Detail;
