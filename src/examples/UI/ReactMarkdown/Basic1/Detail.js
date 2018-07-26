96ea837bf52318e3375209c386c0d8553fda5264
liuwenzhu <547485261@qq.com>
2018-07-26 17:04:32 +0800

    添加markdown组件
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/examples/UI/ReactMarkdown/Basic1/Detail.js
+++ src/examples/UI/ReactMarkdown/Basic1/Detail.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { CountDown } from 'quant-ui';
-
-const targetTime = new Date().getTime() + 3900000;
+import { ReactMarkdown ,Input} from 'quant-ui';
+const { TextArea } = Input;
 class Detail extends Component {
-
+	constructor(props){
+		super(props)
+		this.state = {
+			value:""
+		}
+	}
+	onChange = (e) =>{
+		this.setState({
+			value:e.target.value
+		})
+	}
 	render() {
 		return (
 			<div>
-				 <CountDown style={{ fontSize: 20 }} target={targetTime} />
+				<div style={{marginBottom:"20px"}}>
+					<h2>请输入Markdown语法</h2>
+					<TextArea onChange = {this.onChange} value={this.state.value} placeholder="请输入Markdown语法" autosize={{minRows:8}} />
+				</div>
+				<ReactMarkdown source={this.state.value} />
 			</div>
 		);
 	}
