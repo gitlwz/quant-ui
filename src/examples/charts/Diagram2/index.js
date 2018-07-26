b526a84bdb0d63a2d5f11ee8d6fb8e115b28ea65
liuwenzhu <547485261@qq.com>
2018-07-26 14:05:14 +0800

    修改首页
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/examples/charts/Diagram2/index.js
+++ src/examples/charts/Diagram2/index.js
@@ -12,7 +12,7 @@ class App extends Component {
     render() {
         return (
             <div style={{ padding: "20px" }}>
-                <h1>关系图</h1>
+                <h1>组织结构图</h1>
                 <Tabs tabPosition={"right"}>
                     <TabPane tab="基本" key="1">
                         <Card >
