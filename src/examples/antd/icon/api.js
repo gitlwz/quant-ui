19fa9ac62abf853ba218c270f68cbcdf0cb69c5b
yueyong <1397604589@qq.com>
2018-07-30 15:36:33 +0800

    icon 文档
    Signed-off-by: yueyong <1397604589@qq.com>

--- src/examples/antd/icon/api.js
+++ src/examples/antd/icon/api.js
@@ -5,11 +5,27 @@ const api = [
 		api: [
 			{
 				key: "1",
-				parameter: "disabled",  //字段名
+				parameter: "spin",  //字段名
 				type: 'boolean',	//类型
 				must: '',				//是否必填
 				default: 'false',							//默认值
-				instructions: '按钮失效状态'	//描述
+				instructions: '是否有旋转动画'	//描述
+			},
+			{
+				key: "2",
+				parameter: "style",  //字段名
+				type: 'object',	//类型
+				must: '',				//是否必填
+				default: '-',							//默认值
+				instructions: '	设置图标的样式，例如 fontSize 和 color'	//描述
+			},
+			{
+				key: "3",
+				parameter: "type",  //字段名
+				type: 'string',	//类型
+				must: '',				//是否必填
+				default: '-',							//默认值
+				instructions: '图标类型'	//描述
 			},
 		]
 	}
--- src/examples/antd/icon/index.js
+++ src/examples/antd/icon/index.js
@@ -3,15 +3,25 @@ import { Card, Button, Tabs } from 'quant-ui';
 import Api from '../../API.js';
 import data from './api.js';
 
+
+import Icon_detail from './icon_detail/Detail.js'
 import Code from '../../Code.js';
 const TabPane = Tabs.TabPane;
 class App extends Component {
     render() {
         return (
             <div style={{ padding: "20px" }}>
-                <h1>Icon图标</h1>
-               
-
+                <div>
+                    <h1>Icon图标</h1>
+                    <Card >
+                        <Icon_detail />
+                    </Card>
+                </div>
+                <div>
+                    <h1>API</h1>
+                    <p>由于图标字体本质上还是文字，可以使用 <code>style</code> 和 <code>className</code> 设置图标的大小和颜色。</p>
+                    <Api data={data} />
+                </div>
 
             </div>
         );
