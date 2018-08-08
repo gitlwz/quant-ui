cf143a41ec807fe318a0f9d9c21eb2c06b37048f
liuwenzhu <547485261@qq.com>
2018-08-08 09:44:12 +0800

    add
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/examples/home/analysis/index.js
+++ src/examples/home/analysis/index.js
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 import "./index.less";
 
 import {
-  Row,
-  Col,
-  Icon,
-  NumberInfo,
-  utils,
-  Card,
-  Trend,
-  Tooltip,
-  Progress,
-  Button
+    Row,
+    Col,
+    Icon,
+    NumberInfo,
+    utils,
+    Card,
+    Trend,
+    Tooltip,
+    Progress,
+    Button
 } from "quant-ui";
 import Echarts1 from "./Echart1.js";
 import Echarts2 from "./Echart2.js";
@@ -23,129 +23,129 @@ import Tab4 from "./tab4.js";
 const { currency } = utils;
 
 const dataSource = [
-  {
-    title: (
-      <div>
-        <NumberInfo
-          subTitle={<span>总销售额</span>}
-          total={currency(7777777).format()}
-        />
-        <Trend flag="up">同周比 12%</Trend>
-        <br />
+    {
+        title: (
+            <div>
+                <NumberInfo
+                    subTitle={<span>总销售额</span>}
+                    total={currency(7777777).format()}
+                />
+                <Trend flag="up">同周比 12%</Trend>
+                <br />
+                <Trend flag="down">日环比 11%</Trend>
+            </div>
+        ),
+        footer: (
+            <p>
+                日均销售额 <span> {currency(777770).format()}</span>
+            </p>
+        )
+    },
+    {
+        title: (
+            <div>
+                <NumberInfo subTitle="访问量" total={currency(8846).format()} />
+                <Echarts1 />
+            </div>
+        ),
+        footer: <p>日访问量 1234</p>
+    },
+    {
+        title: (
+            <div>
+                <NumberInfo subTitle="支付笔数" total={currency(6560).format()} />
+                <Echarts2 />
+            </div>
+        ),
+        footer: <p>转化率 60%</p>
+    },
+    {
+        title: (
+            <div>
+                <NumberInfo subTitle="运营活动效果" total="78%" />
+                <Progress percent={78} />
+            </div>
+        ),
+        footer: (
+            <div>
+                <Trend flag="up">同周比 12%</Trend>&nbsp;&nbsp;
         <Trend flag="down">日环比 11%</Trend>
-      </div>
-    ),
-    footer: (
-      <p>
-        日均销售额 <span> {currency(777770).format()}</span>
-      </p>
-    )
-  },
-  {
-    title: (
-      <div>
-        <NumberInfo subTitle="访问量" total={currency(8846).format()} />
-        <Echarts1 />
-      </div>
-    ),
-    footer: <p>日访问量 1234</p>
-  },
-  {
-    title: (
-      <div>
-        <NumberInfo subTitle="支付笔数" total={currency(6560).format()} />
-        <Echarts2 />
-      </div>
-    ),
-    footer: <p>转化率 60%</p>
-  },
-  {
-    title: (
-      <div>
-        <NumberInfo subTitle="运营活动效果" total="78%" />
-        <Progress percent={78} />
-      </div>
-    ),
-    footer: (
-      <div>
-        <Trend flag="up">同周比 12%</Trend>&nbsp;&nbsp;
-        <Trend flag="down">日环比 11%</Trend>
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 ];
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <Row
-          gutter={16}
-          type="flex"
-          justify="space-between"
-          style={{ margin: 20 }}
-        >
-          {dataSource.map(element => (
-            <Col xs={24} sm={12} lg={12} xl={6}>
-              <Card
-                title={element.title}
-                extra={
-                  <Tooltip title="指标说明">
-                    <Icon type="info-circle" />
-                  </Tooltip>
-                }
-              >
-                {element.footer}
-              </Card>
-            </Col>
-          ))}
-        </Row>
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <Row
+                    gutter={16}
+                    type="flex"
+                    justify="space-between"
+                    style={{ margin: 20 }}
+                >
+                    {dataSource.map(element => (
+                        <Col xs={24} sm={12} lg={12} xl={6}>
+                            <Card
+                                title={element.title}
+                                extra={
+                                    <Tooltip title="指标说明">
+                                        <Icon type="info-circle" />
+                                    </Tooltip>
+                                }
+                            >
+                                {element.footer}
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
 
-        <Row
-          gutter={16}
-          type="flex"
-          justify="space-between"
-          style={{ margin: 20 }}
-        >
-          <Col span={24}>
-            <Card>
-              <Tab />
-            </Card>
-          </Col>
-        </Row>
+                <Row
+                    gutter={16}
+                    type="flex"
+                    justify="space-between"
+                    style={{ margin: 20 }}
+                >
+                    <Col span={24}>
+                        <Card>
+                            <Tab />
+                        </Card>
+                    </Col>
+                </Row>
 
-        <Row
-          gutter={16}
-          type="flex"
-          justify="space-between"
-          style={{ margin: 20 }}
-        >
-          <Col lg={24} xl={12}>
-            <Tab2 />
-          </Col>
-          <Col lg={24} xl={12}>
-            <Tab3 />
-          </Col>
-        </Row>
+                <Row
+                    gutter={16}
+                    type="flex"
+                    justify="space-between"
+                    style={{ margin: 20 }}
+                >
+                    <Col lg={24} xl={12}>
+                        <Tab2 />
+                    </Col>
+                    <Col lg={24} xl={12}>
+                        <Tab3 />
+                    </Col>
+                </Row>
 
-        <Row
-          gutter={16}
-          type="flex"
-          justify="space-between"
-          style={{ margin: 20 }}
-        >
-          <Col span={24}>
-            <Card>
-              <Tab4 />
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+                <Row
+                    gutter={16}
+                    type="flex"
+                    justify="space-between"
+                    style={{ margin: 20 }}
+                >
+                    <Col span={24}>
+                        <Card>
+                            <Tab4 />
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/examples/home/analysis/tabPane1.js
+++ src/examples/home/analysis/tabPane1.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import {
-  Radio,
-  RangePicker,
-  Tabs,
-  DatePicker,
-  List,
-  Icon,
-  utils
+    Radio,
+    RangePicker,
+    Tabs,
+    DatePicker,
+    List,
+    Icon,
+    utils
 } from "quant-ui";
 import Chart3 from "./Echart3.js";
 import Chart4 from "./Echart4.js";
@@ -17,152 +17,152 @@ const RadioButton = Radio.Button;
 const ListItem = List.Item;
 const { currency } = utils;
 const data = [
-  {
-    icon: "smile-o",
-    name: "工专路 0 号店",
-    num: currency(323234).format()
-  },
-  {
-    icon: "smile-o",
-    name: "工专路 1 号店",
-    num: currency(323234).format()
-  },
-  {
-    icon: "smile-o",
-    name: "工专路 2 号店",
-    num: currency(323234).format()
-  },
-  {
-    icon: "smile-o",
-    name: "工专路 3 号店",
-    num: currency(323234).format()
-  },
-  {
-    icon: "smile-o",
-    name: "工专路 4 号店",
-    num: currency(323234).format()
-  },
-  {
-    icon: "smile-o",
-    name: "工专路 5 号店",
-    num: currency(323234).format()
-  },
-  {
-    icon: "smile-o",
-    name: "工专路 6 号店",
-    num: currency(323234).format()
-  },
-  {
-    icon: "smile-o",
-    name: "工专路 7 号店",
-    num: currency(323234).format()
-  }
+    {
+        icon: "smile-o",
+        name: "工专路 0 号店",
+        num: currency(323234).format()
+    },
+    {
+        icon: "smile-o",
+        name: "工专路 1 号店",
+        num: currency(323234).format()
+    },
+    {
+        icon: "smile-o",
+        name: "工专路 2 号店",
+        num: currency(323234).format()
+    },
+    {
+        icon: "smile-o",
+        name: "工专路 3 号店",
+        num: currency(323234).format()
+    },
+    {
+        icon: "smile-o",
+        name: "工专路 4 号店",
+        num: currency(323234).format()
+    },
+    {
+        icon: "smile-o",
+        name: "工专路 5 号店",
+        num: currency(323234).format()
+    },
+    {
+        icon: "smile-o",
+        name: "工专路 6 号店",
+        num: currency(323234).format()
+    },
+    {
+        icon: "smile-o",
+        name: "工专路 7 号店",
+        num: currency(323234).format()
+    }
 ];
 class ExtraContent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "year"
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: "year"
+        };
+    }
+    onChange = e => {
+        this.setState({
+            value: e.target.value
+        });
     };
-  }
-  onChange = e => {
-    this.setState({
-      value: e.target.value
-    });
-  };
-  handleChange = () => {
-    const { value } = this.state;
-  };
-  render() {
-    //const {value}=this.state;
-    return (
-      <Tabs
-        defaultKey="1"
-        tabBarExtraContent={
-          <div>
-            <RadioGroup onChange={this.onChange} value={this.state.value}>
-              <RadioButton value="today" style={{ marginLeft: 10 }}>
-                today
-              </RadioButton>
-              <RadioButton value="week" style={{ marginLeft: 10 }}>
-                week
-              </RadioButton>
-              <RadioButton value="month" style={{ marginLeft: 10 }}>
-                month
-              </RadioButton>
-              <RadioButton value="year" style={{ marginLeft: 10 }}>
-                year
-              </RadioButton>
-            </RadioGroup>
-            <DatePicker style={{ marginLeft: 10 }} />
-          </div>
-        }
-      >
-        <TabPane tab="销售额" key="1">
-          <div
-            style={{
-              width: "70%",
-              float: "left"
-            }}
-          >
-            <Chart3 />
-          </div>
-          <div style={{ float: "left", width: "30%" }}>
-            <List
-              header={
-                <div>
-                  <b>门店销售额排名</b>
-                </div>
-              }
-              dataSource={data}
-              split={false}
-              style={{ width: "100%" }}
-              renderItem={item => (
-                <ListItem>
-                  <List.Item.Meta
-                    avatar={<Icon type={item.icon} />}
-                    title={item.name}
-                  />
-                  <div>{item.num}</div>
-                </ListItem>
-              )}
-            />
-          </div>
-        </TabPane>
-        <TabPane tab="访问量" key="2">
-          <div
-            style={{
-              width: "70%",
-              float: "left"
-            }}
-          >
-            <Chart4/>
-          </div>
-          <div style={{ float: "left", width: "30%" }}>
-            <List
-              header={
-                <div>
-                  <b>门店访问量排名</b>
-                </div>
-              }
-              dataSource={data}
-              split={false}
-              style={{ width: "100%" }}
-              renderItem={item => (
-                <ListItem>
-                  <List.Item.Meta
-                    avatar={<Icon type={item.icon} />}
-                    title={item.name}
-                  />
-                  <div>{item.num}</div>
-                </ListItem>
-              )}
-            />
-          </div>
-        </TabPane>
-      </Tabs>
-    );
-  }
+    handleChange = () => {
+        const { value } = this.state;
+    };
+    render() {
+        //const {value}=this.state;
+        return (
+            <Tabs
+                defaultKey="1"
+                tabBarExtraContent={
+                    <div>
+                        <RadioGroup onChange={this.onChange} value={this.state.value}>
+                            <RadioButton value="today" style={{ marginLeft: 10 }}>
+                                today
+                            </RadioButton>
+                            <RadioButton value="week" style={{ marginLeft: 10 }}>
+                                week
+                            </RadioButton>
+                            <RadioButton value="month" style={{ marginLeft: 10 }}>
+                                month
+                            </RadioButton>
+                            <RadioButton value="year" style={{ marginLeft: 10 }}>
+                                year
+                            </RadioButton>
+                        </RadioGroup>
+                        <DatePicker style={{ marginLeft: 10 }} />
+                    </div>
+                }
+            >
+                <TabPane tab="销售额" key="1">
+                    <div
+                        style={{
+                            width: "70%",
+                            float: "left"
+                        }}
+                    >
+                        <Chart3 />
+                    </div>
+                    <div style={{ float: "left", width: "30%" }}>
+                        <List
+                            header={
+                                <div>
+                                    <b>门店销售额排名</b>
+                                </div>
+                            }
+                            dataSource={data}
+                            split={false}
+                            style={{ width: "100%" }}
+                            renderItem={item => (
+                                <ListItem>
+                                    <List.Item.Meta
+                                        avatar={<Icon type={item.icon} />}
+                                        title={item.name}
+                                    />
+                                    <div>{item.num}</div>
+                                </ListItem>
+                            )}
+                        />
+                    </div>
+                </TabPane>
+                <TabPane tab="访问量" key="2">
+                    <div
+                        style={{
+                            width: "70%",
+                            float: "left"
+                        }}
+                    >
+                        <Chart4 />
+                    </div>
+                    <div style={{ float: "left", width: "30%" }}>
+                        <List
+                            header={
+                                <div>
+                                    <b>门店访问量排名</b>
+                                </div>
+                            }
+                            dataSource={data}
+                            split={false}
+                            style={{ width: "100%" }}
+                            renderItem={item => (
+                                <ListItem>
+                                    <List.Item.Meta
+                                        avatar={<Icon type={item.icon} />}
+                                        title={item.name}
+                                    />
+                                    <div>{item.num}</div>
+                                </ListItem>
+                            )}
+                        />
+                    </div>
+                </TabPane>
+            </Tabs>
+        );
+    }
 }
 
 export default ExtraContent;
