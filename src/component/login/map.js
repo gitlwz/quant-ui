65c06aea571ff7757249f2bf1bd2072867f9fba1
liuwenzhu <547485261@qq.com>
2018-08-15 13:42:55 +0800

    add
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/component/login/map.js
+++ src/component/login/map.js
@@ -3,67 +3,67 @@ import Input from '../input';
 import Icon from '../icon';
 
 const map = {
-  UserName: {
-    component: Input,
-    props: {
-      size: 'large',
-      prefix: <Icon type="user" className={'quant-login-prefixIcon'} />,
-      placeholder: 'admin',
+    UserName: {
+        component: Input,
+        props: {
+            size: 'large',
+            prefix: <Icon type="user" className={'quant-login-prefixIcon'} />,
+            placeholder: '请输入用户名',
+        },
+        rules: [
+            {
+                required: true,
+                message: '请输入用户名!',
+            },
+        ],
     },
-    rules: [
-      {
-        required: true,
-        message: 'Please enter username!',
-      },
-    ],
-  },
-  Password: {
-    component: Input,
-    props: {
-      size: 'large',
-      prefix: <Icon type="lock" className={'quant-login-prefixIcon'} />,
-      type: 'password',
-      placeholder: '888888',
+    Password: {
+        component: Input,
+        props: {
+            size: 'large',
+            prefix: <Icon type="lock" className={'quant-login-prefixIcon'} />,
+            type: 'password',
+            placeholder: '请输入密码',
+        },
+        rules: [
+            {
+                required: true,
+                message: '请输入密码!',
+            },
+        ],
     },
-    rules: [
-      {
-        required: true,
-        message: 'Please enter password!',
-      },
-    ],
-  },
-  Mobile: {
-    component: Input,
-    props: {
-      size: 'large',
-      prefix: <Icon type="mobile" className={'quant-login-prefixIcon'} />,
-      placeholder: 'mobile number',
+    Mobile: {
+        component: Input,
+        props: {
+            size: 'large',
+            prefix: <Icon type="mobile" className={'quant-login-prefixIcon'} />,
+            placeholder: 'mobile number',
+        },
+        rules: [
+            {
+                required: true,
+                message: 'Please enter mobile number!',
+            },
+            {
+                pattern: /^1\d{10}$/,
+                message: 'Wrong mobile number format!',
+            },
+        ],
     },
-    rules: [
-      {
-        required: true,
-        message: 'Please enter mobile number!',
-      },
-      {
-        pattern: /^1\d{10}$/,
-        message: 'Wrong mobile number format!',
-      },
-    ],
-  },
-  Captcha: {
-    component: Input,
-    props: {
-      size: 'large',
-      prefix: <Icon type="mail" className={'quant-login-prefixIcon'} />,
-      placeholder: 'captcha',
+    Captcha: {
+        component: Input,
+        props: {
+            size: 'large',
+            prefix: <Icon type="mail" className={'quant-login-prefixIcon'} />,
+            placeholder: 'captcha',
+        },
+        rules: [
+            {
+                required: true,
+                message: 'Please enter Captcha!',
+            },
+        ],
     },
-    rules: [
-      {
-        required: true,
-        message: 'Please enter Captcha!',
-      },
-    ],
-  },
 };
 
 export default map;
--- src/examples/test/upload/Basic1/Detail.js
+++ src/examples/test/upload/Basic1/Detail.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { Upload, Button, Icon, message  } from 'quant-ui';
+import { Upload, Button, Icon, message } from 'quant-ui';
 import isArray from "lodash/isArray"
 class Detail extends Component {
-    
-	state = {
+
+    state = {
         uploading: false,
-      }
-    
-      handleUpload = (fileList) => {
+    }
+
+    handleUpload = (fileList) => {
         const formData = new FormData();
         fileList.forEach((file) => {
             formData.append('files[]', file);
@@ -15,7 +15,7 @@ class Detail extends Component {
         this.setState({
             uploading: true,
         });
-        console.log("********8",formData,fileList)
+        console.log("********8", formData, fileList)
         // You can use any AJAX library you like
         // reqwest({
         //   url: '//jsonplaceholder.typicode.com/posts/',
@@ -36,14 +36,14 @@ class Detail extends Component {
         //     message.error('upload failed.');
         //   },
         // });
-      }
-    
-      render() {
+    }
+
+    render() {
         const { uploading } = this.state;
         const props = {
             beforeUpload: (file) => {
                 let files = file;
-                if(!isArray(file)){
+                if (!isArray(file)) {
                     files = [file];
                 }
                 this.handleUpload(files)
@@ -51,12 +51,12 @@ class Detail extends Component {
             },
             fileList: [],
         };
-    
+
         return (
             <div>
                 <Upload {...props} disabled={uploading}>
                     <Button loading={uploading}>
-                        {!uploading&&<Icon type="upload"/> }{uploading ? 'Uploading' : 'Select File' }
+                        {!uploading && <Icon type="upload" />}{uploading ? 'Uploading' : 'Select File'}
                     </Button>
                 </Upload>
             </div>
